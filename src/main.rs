cc43885bb90b98e3318c04e919280cf1c2b4d76a
Alex Yang <aleozlx@gmail.com>
2018-10-09 22:32:05 -0500

    ++

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("aml-sub")
+        .version("1.0")
+        .author("Alex Yang <zy5f9@mail.missouri.edu>")
+        .about("Applied Machine Learning job submission")
+        .arg(Arg::with_name("INPUT")
+            .help("Input file")
+            .required(true)
+            .index(1))
+        .get_matches();
 }
+
